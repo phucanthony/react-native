f02d267685063b276db903ff737214e4c54fa034
PHUC-LAPTOP\Phuc Anthony <phuc9h2010@gmail.com>
2017-07-11 18:19:49 +0700

    no message

--- packager/src/Resolver/polyfills/babelHelpers.js
+++ packager/src/Resolver/polyfills/babelHelpers.js
@@ -19,6 +19,35 @@
 
 var babelHelpers = global.babelHelpers = {};
 
+babelHelpers.asyncToGenerator = function (fn) {
+  return function () {
+    var gen = fn.apply(this, arguments);
+    return new Promise(function (resolve, reject) {
+      function step(key, arg) {
+        try {
+          var info = gen[key](arg);
+          var value = info.value;
+        } catch (error) {
+          reject(error);
+          return;
+        }
+
+        if (info.done) {
+          resolve(value);
+        } else {
+          return Promise.resolve(value).then(function (value) {
+            return step("next", value);
+          }, function (err) {
+            return step("throw", err);
+          });
+        }
+      }
+
+      return step("next");
+    });
+  };
+};
+
 babelHelpers.typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
   return typeof obj;
 } : function (obj) {
